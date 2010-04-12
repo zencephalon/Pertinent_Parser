8d9b8b25647fdc085e1dab7fcb592efec7dfd7a1
SlyShy <mkbunday@stanford.edu>
2010-04-12 10:37:06 -0500

    Documentation.

--- pertinent_parser.rb
+++ pertinent_parser.rb
@@ -32,6 +32,7 @@ module PertinentParser
             i = PertinentParser::range_i(@target, words, @position)
         end
 
+        # Recursively apply the children and the rule to a string.
         def apply s
             t, st = @target.dup, s.dup
             @children.each do |child|
@@ -43,13 +44,21 @@ module PertinentParser
             st
         end
 
+        # Compose a rule.
+        # For entirely overlapping rules or non-overlapping rules this operation is commutative.
+        # It is _not_ commutative for partially overlapping rules. The second rule will take precedance,
+        # that is, it will break the first rule into two parts to preserve itself.
         def + rule, context
             this_match = range(context).to_a
             that_match = rule.range(context).to_a
             intersection = this_match & that_match
-            #first case, the rule is entirely inside
+
+            # Case: the rules do not intersect at all. They can both be applied safely separately, as their
+            # target areas are entirely distinct.
             if intersection.empty?
                 [self, rule]
+            # Case: the second rule is entirely inside the first. It will become a child of the first rule,
+            # but first it must be recursively added to the children of the first rule.
             elsif intersection == that_match
                 if @children.empty?
                     @children << rule
@@ -61,9 +70,13 @@ module PertinentParser
                     @children = children.uniq
                 end
                 [self]
+            # Case: the first rule is entirely inside the first. This is symmetrical with the previous case.
             elsif intersection == this_match
                 rule.+(self, context)
                 [rule]
+            # Case: the two rules have non-trivial intersection. The part of the first rule inside the second
+            # rule is added as a child to the second rule. The part of the first rule outside the second rule
+            # may be safely applied on its own.
             else
                 inner_target = @target[(intersection.first..intersection.last)] 
                 r_in = Rule.new(inner_target, find_position(inner_target, intersection, context), &@function)
@@ -76,12 +89,22 @@ module PertinentParser
         end
     end
 
+    # A transform is a top level collection of rules and an input.
+    # Rules added to a transform will operate with the input as their
+    # context. This is important in the composition stage of adding 
+    # rules.
     class Transform
         attr_accessor :rules, :input
         def initialize input
             @input = input
             @rules = []
         end
+
+        # Short-hand method for composing new rules.
+        # Takes two forms. Either add("target", "<tag attrs>")
+        # which will create a function that maps "string" to
+        # "<tag attrs>string</tag>", or add("target") {|s| do_whatever}
+        # which takes a manually specified function.
         def add string, tag="", pos=1, &func
             if func.nil?
                 func = proc do |s|
@@ -90,6 +113,8 @@ module PertinentParser
             end
             add_rule(string.split(""), pos, &func)
         end
+
+        # Same as the block form of the short-hand method.
         def add_rule target, position=1, &function 
             r = Rule.new(target, position, &function)
             return false if r.range(@input).end > @input.size
@@ -104,16 +129,21 @@ module PertinentParser
             end
             true
         end
+
+        # Apply each rule to the input, give the output.
         def apply
             c = @input.dup
             @rules.each {|r| c = r.apply(c)}
             c.join
         end
+
+        # Return the input.
         def text
             @input.join
         end
     end
 
+    # Recursive helper function. See wrapper.
     def self.r_range target, words, depth
         if words.empty?
             -1
@@ -123,10 +153,14 @@ module PertinentParser
             1 + r_range(target, words.drop(1), depth)
         end
     end
+
+    # Returns the range of the ith occurence of target in words.
     def self.range_i target, words, i
         a = r_range(target, words, i)
         (a...a + target.size)
     end
+
+    # Finds which occurence of target happens in the range in words.
     def self.find_position target, range, words
         i = 1
         while (m = range_i(target, words, i)).end <= words.size
@@ -135,12 +169,16 @@ module PertinentParser
         end
     end
 
+    # Creates a transform instance given HTML. The input of the transform
+    # will be stripped down plain text, and the rules will be such that
+    # applying the transform will return to the original HTML.
     def self.html(input)
        t = Transform.new(extract_text(input))
        html_transform(t, input)
        t
     end
 
+    # Extract rules from HTML tag occurences.
     def self.html_transform(t, input)
         #left, open_tag, contents, close_tag, right = 
         matched = match(input)
@@ -154,6 +192,7 @@ module PertinentParser
         end
     end
 
+    # Return the plain text from an HTML document.
     def self.extract_text(input)
         if (matched = match(input))[1] == ""
             matched[0] == "" ? [] : matched[0].split("")
@@ -162,6 +201,7 @@ module PertinentParser
         end
     end
 
+    # Match a pair of tags.
     def self.match(html=@html)
         first, open_tag, right = html.partition(/<.*?>/)
         score, contents, close_tag = 1, "", ""
