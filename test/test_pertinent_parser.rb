de7f40f246b6ecbbffb8114c33f104fd968ff0d7
Matthew Bunday <zenceph@gmail.com>
2014-04-25 06:40:35 -0400

    Basic parse test.

--- test/test_pertinent_parser.rb
+++ test/test_pertinent_parser.rb
@@ -2,7 +2,8 @@ require 'minitest/autorun'
 require 'pertinent_parser'
 
 class PertinentParserTest < MiniTest::Test
-  def test_basic_wrap
-    assert(true)
+  def test_basic_parse
+    assert_equal(html("A <i>sentence with</i> some markup."), "A sentence with some markup.")
+    assert_equal(html("A <i>sentence with</i> <b>some more</b> markup."), "A sentence with some more markup.")
   end
 end
