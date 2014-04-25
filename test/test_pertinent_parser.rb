d79365142ae88d722ef31322b1aef462acf3fdb0
Matthew Bunday <zenceph@gmail.com>
2014-04-25 06:59:42 -0400

    Test basic wrap_in

--- test/test_pertinent_parser.rb
+++ test/test_pertinent_parser.rb
@@ -3,7 +3,13 @@ require 'pertinent_parser'
 
 class PertinentParserTest < MiniTest::Test
   def test_basic_parse
-    assert_equal(PertinentParser.html("A <i>sentence with</i> some markup."), "A sentence with some markup.")
-    assert_equal(PertinentParser.html("A <i>sentence with</i> <b>some more</b> markup."), "A sentence with some more markup.")
+    assert_equal("A sentence with some markup.", PertinentParser.html("A <i>sentence with</i> some markup."))
+    assert_equal("A sentence with some more markup.", PertinentParser.html("A <i>sentence with</i> <b>some more</b> markup."))
+  end
+
+  def test_basic_wrap_in
+    h = PertinentParser.html("A sentence with no markup.")
+    h.wrap_in("<b>", "with no")
+    assert_equal("A sentence <b>with no</b> markup.", h.apply)
   end
 end
